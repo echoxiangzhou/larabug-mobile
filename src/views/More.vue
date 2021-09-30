41f116099feb44c601debb6b4d8be7b48e029f1e
Sebastiaan Kloos <sebastiaan@codecycler.com>
2021-09-30 17:11:08 +0200

    Added some content to the more page

--- src/views/More.vue
+++ src/views/More.vue
@@ -14,6 +14,42 @@
 
             <div class="p-4 space-y-5">
                 <div>
+                    <ion-list lines="none">
+                        <ion-item detail @click="openUrl('https://www.larabug.com/discord?utm_source=com.larabug.app')">
+                            <ion-icon class="text-indigo-600" slot="start" :icon="logoDiscord"></ion-icon>
+
+                            <ion-label>
+                                Join our Discord server
+                            </ion-label>
+                        </ion-item>
+
+                        <ion-item detail @click="openUrl('https://github.com/sponsors/Cannonb4ll')">
+                            <ion-icon class="text-yellow-400" slot="start" :icon="happy"></ion-icon>
+
+                            <ion-label>
+                                Become a sponsor
+                            </ion-label>
+                        </ion-item>
+
+                        <ion-item detail @click="openUrl('https://github.com/larabug')">
+                            <ion-icon slot="start" :icon="logoGithub"></ion-icon>
+
+                            <ion-label>
+                                LaraBug on Github
+                            </ion-label>
+                        </ion-item>
+
+                        <ion-item detail @click="openUrl('https://twitter.com/LaraBugEN')">
+                            <ion-icon class="text-blue-400" slot="start" :icon="logoTwitter"></ion-icon>
+
+                            <ion-label>
+                                LaraBug on Twitter
+                            </ion-label>
+                        </ion-item>
+                    </ion-list>
+                </div>
+
+                <div>
                     <h2 class="text-lg">Shout out to our awesome sponsors</h2>
                 </div>
 
@@ -28,13 +64,14 @@
 </template>
 
 <script>
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonIcon, IonLabel } from '@ionic/vue';
+import { logoDiscord, happy, logoGithub, logoTwitter } from "ionicons/icons";
 import config from "@/config";
 import SponsorService from "@/services/SponsorService";
 
 export default  {
     name: 'More',
-    components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+    components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonList, IonItem, IonIcon, IonLabel },
     computed: {
         client() {
             return new SponsorService();
@@ -58,6 +95,14 @@ export default  {
     },
     ionViewWillEnter() {
         this.getData();
+    },
+    setup() {
+        return {
+            logoDiscord,
+            happy,
+            logoGithub,
+            logoTwitter,
+        }
     },
 }
 </script>
